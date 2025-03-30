803fdda8db2940c8de1165f26bf21fa4d04c0af4
Edward Lim <edwardlim@occamlab.com.sg>
2025-03-30 22:07:34 +0800

    add diff env

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,38 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      exclude: [
-        'node_modules/',
-        'src/test/setup.ts',
-        '**/*.d.ts',
-        '**/*.test.{js,jsx,ts,tsx}',
-        '**/*.config.{js,ts}',
-      ],
-      include: [
-        'src/**/*.{js,jsx,ts,tsx}',
-      ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  
+  return {
+    base: env.VITE_BASE_PATH,
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./src/test/setup.ts'],
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'json', 'html'],
+        exclude: [
+          'node_modules/',
+          'src/test/setup.ts',
+          '**/*.d.ts',
+          '**/*.test.{js,jsx,ts,tsx}',
+          '**/*.config.{js,ts}',
+        ],
+        include: [
+          'src/**/*.{js,jsx,ts,tsx}',
+        ],
+      },
+      include: ['src/**/*.{test,spec}.{js,jsx,ts,tsx}'],
+      reporters: ['default', ['vitest-sonar-reporter', {
+        outputFile: './test-report.xml',
+        outputDirectory: './',
+        reportName: 'test-report.xml'
+      }]]
     },
-    include: ['src/**/*.{test,spec}.{js,jsx,ts,tsx}'],
-    reporters: ['default', ['vitest-sonar-reporter', {
-      outputFile: './test-report.xml',
-      outputDirectory: './',
-      reportName: 'test-report.xml'
-    }]]
-  },
+  }
 })
