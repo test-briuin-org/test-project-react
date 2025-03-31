e98a0495aa5f31612f8972e82c1b54df1b0a7140
Edward Lim <edwardlim@occamlab.com.sg>
2025-03-31 11:37:45 +0800

    fix cypress

---
+++ cypress/support/e2e.js
@@ -0,0 +1,6 @@
+// You can add global configurations and behavior that modifies Cypress
+// Import commands.js using ES2015 syntax:
+import './commands';
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands') 
